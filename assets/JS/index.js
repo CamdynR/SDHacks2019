d7d4183fba96154d2b500f0a3c64a0bb246f9403
CamdynR <rasque.camdyn@gmail.com>
2019-10-26 12:52:44 -0700

    Text analysis is working

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,5 +1,22 @@
 // Main JavaScript Page
 
-function testMethod() {
-  alert("Hello Sahba :)");
+// Initialize the Amazon Cognito credentials provider
+AWS.config.region = 'us-east-2'; // Region/
+AWS.config.credentials = new AWS.Credentials();
+AWS.config.credentials.accessKeyId = keyA;
+AWS.config.credentials.secretAccessKey = keyS;
+
+var comprehend = new AWS.Comprehend({ apiVersion: '2017-11-27' });
+
+function parseText(event) {
+  event.preventDefault();
+
+  var parseParams = {
+    LanguageCode: 'en',
+    Text: 'My name is Camdyn'
+  };
+  comprehend.detectKeyPhrases(parseParams, function (err, data) {
+    if (err) console.log(err, err.stack);
+    else console.log(data);
+  });
 }
